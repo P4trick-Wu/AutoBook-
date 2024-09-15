fb8f1618087c75a220379bc6b245f14f7aa056ca
P4trick-Wu <patrickwu.2004@gmail.com>
2024-09-15 11:40:34 -0400

    Add project files.

---
+++ AutoBook+/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+Console.WriteLine("Hello, World!");
+
+//Create driver reference for the chrome
+IWebDriver driver = new ChromeDriver();
+
+//Navigate to url
+driver.Navigate().GoToUrl("https://www.google.ca");
+
+//Identify google search box
+IWebElement element = driver.FindElement(By.Name("q"));
+
+//Enter value into search box
+element.SendKeys("overwatch");
+
+Thread.Sleep(1000);
+
+//Find search button
+element = driver.FindElement(By.Name("btnK"));
+
+//Click button
+element.Click();
+Thread.Sleep(3000);
+
+//close browser
+driver.Close();
+Console.WriteLine("Test case ended");
